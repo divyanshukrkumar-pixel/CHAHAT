4f8351a178380a9e02f9f8a6cbe672da45a2650b
divyanshukrkumar-pixel <divyanshukr.kumar@gmail.com>
2025-12-27 10:21:22 +0530

    Update afk.py

--- BrandrdXMusic/plugins/bot/afk.py
+++ BrandrdXMusic/plugins/bot/afk.py
@@ -1,7 +1,10 @@
-import time, re
+import time
+import re
+
 from pyrogram.enums import MessageEntityType
 from pyrogram import filters
 from pyrogram.types import Message
+
 from BrandrdXMusic import app
 from BrandrdXMusic.mongo.readable_time import get_readable_time
 from BrandrdXMusic.mongo.afkdb import add_afk, is_afk, remove_afk
@@ -11,28 +14,35 @@ from BrandrdXMusic.mongo.afkdb import add_afk, is_afk, remove_afk
 async def active_afk(_, message: Message):
     if message.sender_chat:
         return
+
     user_id = message.from_user.id
     verifier, reasondb = await is_afk(user_id)
+
     if verifier:
         await remove_afk(user_id)
         try:
             afktype = reasondb["type"]
             timeafk = reasondb["time"]
-            data = reasondb["data"]
             reasonafk = reasondb["reason"]
-            seenago = get_readable_time((int(time.time() - timeafk)))
+            seenago = get_readable_time(int(time.time() - timeafk))
+
             if afktype == "text":
-                send = await message.reply_text(
-                    f"**{message.from_user.first_name}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ ᴀɴᴅ ᴡᴀs ᴀᴡᴀʏ ғᴏʀ {seenago}",
+                await message.reply_text(
+                    f"**{message.from_user.first_name}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ "
+                    f"ᴀɴᴅ ᴡᴀs ᴀᴡᴀʏ ғᴏʀ {seenago}",
                     disable_web_page_preview=True,
                 )
-            if afktype == "text_reason":
-                send = await message.reply_text(
-                    f"**{message.from_user.first_name}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ ᴀɴᴅ ᴡᴀs ᴀᴡᴀʏ ғᴏʀ {seenago}\n\nʀᴇᴀsᴏɴ: `{reasonafk}`",
+
+            elif afktype == "text_reason":
+                await message.reply_text(
+                    f"**{message.from_user.first_name}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ "
+                    f"ᴀɴᴅ ᴡᴀs ᴀᴡᴀʏ ғᴏʀ {seenago}\n\n"
+                    f"ʀᴇᴀsᴏɴ: `{reasonafk}`",
                     disable_web_page_preview=True,
                 )
-         except Exception:
-            send = await message.reply_text(
+
+        except Exception:
+            await message.reply_text(
                 f"**{message.from_user.first_name}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ",
                 disable_web_page_preview=True,
             )
@@ -45,14 +55,15 @@ async def active_afk(_, message: Message):
             "reason": None,
         }
     elif len(message.command) > 1 and not message.reply_to_message:
-        _reason = (message.text.split(None, 1)[1].strip())[:100]
+        _reason = message.text.split(None, 1)[1][:100]
         details = {
             "type": "text_reason",
             "time": time.time(),
             "data": None,
             "reason": _reason,
         }
-   
+    else:
+        return
 
     await add_afk(user_id, details)
     await message.reply_text(f"{message.from_user.first_name} ɪs ɴᴏᴡ ᴀғᴋ!")
@@ -65,21 +76,13 @@ chat_watcher_group = 1
     ~filters.me & ~filters.bot & ~filters.via_bot,
     group=chat_watcher_group,
 )
-async def chat_watcher_func(_, message):
+async def chat_watcher_func(_, message: Message):
     if message.sender_chat:
         return
+
     userid = message.from_user.id
     user_name = message.from_user.first_name
-    if message.entities:
-        possible = ["/afk", f"/afk@{app.username}"]
-        message_text = message.text or message.caption
-        for entity in message.entities:
-            if entity.type == MessageEntityType.BOT_COMMAND:
-                if (message_text[0 : 0 + entity.length]).lower() in possible:
-                    return
-
     msg = ""
-    replied_user_id = 0
 
     verifier, reasondb = await is_afk(userid)
     if verifier:
@@ -87,101 +90,21 @@ async def chat_watcher_func(_, message):
         try:
             afktype = reasondb["type"]
             timeafk = reasondb["time"]
-            data = reasondb["data"]
             reasonafk = reasondb["reason"]
-            seenago = get_readable_time((int(time.time() - timeafk)))
+            seenago = get_readable_time(int(time.time() - timeafk))
+
             if afktype == "text":
-                msg += f"**{user_name[:25]}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ ᴀɴᴅ ᴡᴀs ᴀᴡᴀʏ ғᴏʀ {seenago}\n\n"
-            if afktype == "text_reason":
-                msg += f"**{user_name[:25]}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ ᴀɴᴅ ᴡᴀs ᴀᴡᴀʏ ғᴏʀ {seenago}\n\nʀᴇᴀsᴏɴ: `{reasonafk}`\n\n"
-           
-        except:
+                msg += f"**{user_name[:25]}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ (ᴀғᴋ {seenago})\n\n"
+            elif afktype == "text_reason":
+                msg += (
+                    f"**{user_name[:25]}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ (ᴀғᴋ {seenago})\n\n"
+                    f"ʀᴇᴀsᴏɴ: `{reasonafk}`\n\n"
+                )
+        except Exception:
             msg += f"**{user_name[:25]}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ\n\n"
 
-    if message.reply_to_message:
+    if msg:
         try:
-            replied_first_name = message.reply_to_message.from_user.first_name
-            replied_user_id = message.reply_to_message.from_user.id
-            verifier, reasondb = await is_afk(replied_user_id)
-            if verifier:
-                try:
-                    afktype = reasondb["type"]
-                    timeafk = reasondb["time"]
-                    data = reasondb["data"]
-                    reasonafk = reasondb["reason"]
-                    seenago = get_readable_time((int(time.time() - timeafk)))
-                    if afktype == "text":
-                        msg += (
-                            f"**{replied_first_name[:25]}** ɪs ᴀғᴋ sɪɴᴄᴇ {seenago}\n\n"
-                        )
-                    if afktype == "text_reason":
-                        msg += f"**{replied_first_name[:25]}** ɪs ᴀғᴋ sɪɴᴄᴇ {seenago}\n\nʀᴇᴀsᴏɴ: `{reasonafk}`\n\n"
-                  
-                except Exception:
-                    msg += f"**{replied_first_name}** ɪs ᴀғᴋ,\nᴩᴀᴛᴀ ɴɪ ʙᴄ ᴋᴀʙ sᴇ\n\n"
-        except:
+            await message.reply_text(msg, disable_web_page_preview=True)
+        except Exception:
             pass
-
-    if message.entities:
-        entity = message.entities
-        j = 0
-        for x in range(len(entity)):
-            if (entity[j].type) == MessageEntityType.MENTION:
-                found = re.findall("@([_0-9a-zA-Z]+)", message.text)
-                try:
-                    get_user = found[j]
-                    user = await app.get_users(get_user)
-                    if user.id == replied_user_id:
-                        j += 1
-                        continue
-                except:
-                    j += 1
-                    continue
-                verifier, reasondb = await is_afk(user.id)
-                if verifier:
-                    try:
-                        afktype = reasondb["type"]
-                        timeafk = reasondb["time"]
-                        data = reasondb["data"]
-                        reasonafk = reasondb["reason"]
-                        seenago = get_readable_time((int(time.time() - timeafk)))
-                        if afktype == "text":
-                            msg += (
-                                f"**{user.first_name[:25]}** ɪs ᴀғᴋ sɪɴᴄᴇ {seenago}\n\n"
-                            )
-                        if afktype == "text_reason":
-                            msg += f"**{user.first_name[:25]}** ɪs ᴀғᴋ sɪɴᴄᴇ {seenago}\n\nʀᴇᴀsᴏɴ: `{reasonafk}`\n\n"
-                       
-                    except:
-                        msg += f"**{user.first_name[:25]}** ɪs ᴀғᴋ\n\n"
-            elif (entity[j].type) == MessageEntityType.TEXT_MENTION:
-                try:
-                    user_id = entity[j].user.id
-                    if user_id == replied_user_id:
-                        j += 1
-                        continue
-                    first_name = entity[j].user.first_name
-                except:
-                    j += 1
-                    continue
-                verifier, reasondb = await is_afk(user_id)
-                if verifier:
-                    try:
-                        afktype = reasondb["type"]
-                        timeafk = reasondb["time"]
-                        data = reasondb["data"]
-                        reasonafk = reasondb["reason"]
-                        seenago = get_readable_time((int(time.time() - timeafk)))
-                        if afktype == "text":
-                            msg += f"**{first_name[:25]}** is ᴀғᴋ sɪɴᴄᴇ {seenago}\n\n"
-                        if afktype == "text_reason":
-                            msg += f"**{first_name[:25]}** ɪs ᴀғᴋ sɪɴᴄᴇ {seenago}\n\nʀᴇᴀsᴏɴ: `{reasonafk}`\n\n"
-                      
-                    except:
-                        msg += f"**{first_name[:25]}** ɪs ᴀғᴋ\n\n"
-            j += 1
-    if msg != "":
-        try:
-            send = await message.reply_text(msg, disable_web_page_preview=True)
-        except:
-            return
